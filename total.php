d68c298aec1b9339994357780c51756e66e01325
o5v3 <33675019+o5v3@users.noreply.github.com>
2019-11-22 12:13:30 -0400

    Implements update functionality
    Implements an "Update" view, where you can see each student individually, change their fields, delete them and save those changes on the file.

--- total.php
+++ total.php
@@ -19,9 +19,12 @@
             echo "<input type='submit' name='state' value='Show students'>";
             echo "<input type='hidden' name='insert' value='Return'>";
             echo "</form>";
+            echo "<form method='POST'>";
+            echo "<input type='submit' name='insert' value='Update students'>";
+            echo "</form>";
             //Si quieres cambiar el archivo del codigo fuente, cambia el nombre aqui.
             $source = fopen("file.txt", "r");
-            echo call_user_func("parser", $source);
+            echo parser($source);
             fclose($source);
             //Permite cambiar el archivo fuente sin que queden residuos del archivo anterior.
             session_unset();
@@ -50,6 +53,64 @@
             echo "</form>";
         };
 
+        function viewStudent($data, $studentNum) {
+            if (!isset($data[$studentNum])) {
+                if (isset($data[$studentNum - 1])) {
+                    viewStudent($data, $studentNum -1);
+                    return;
+                } else {
+                    echo "<form method='POST'>";
+                        echo "<input type='submit' name='insert' value='Return'></input>";
+                    echo "</form>";
+                    return;
+                }
+            }
+            echo createTable($data[$studentNum]);
+            echo "<form method='POST'>";
+            if ($studentNum != 1){
+                echo "<input type='submit' name='state' value='Anterior'></input>";
+            };
+            if ($studentNum != count($data)) {
+                echo "<input type='submit' name='state' value='Siguiente'></input>";
+            };
+            echo "<input type='hidden' name='currentStudent' value='$studentNum'></input>";     
+            echo "<input type='hidden' name='insert' value='Update students'></input>";                    
+            echo "</form>";
+            echo "<form method='POST'>";
+                echo "<input type='submit' name='insert' value='Return'></input>";
+            echo "</form>";
+            echo "<form method='POST'>";
+                echo "<input type='hidden' name='currentStudent' value='$studentNum'></input>";  
+                echo "<input type='submit' name='insert' value='Update'></input>";
+            echo "</form>";
+            echo "<form method='POST'>";
+                echo "<input type='hidden' name='currentStudent' value='$studentNum'></input>";  
+                echo "<input type='submit' name='insert' value='Delete student'></input>";
+            echo "</form>";
+        };    
+        
+        function updateStudent($data, $studentNum) {
+            $form = "<form method='POST'>";
+            $exclusive = false;
+            foreach ($data[$studentNum] as $property => $value) {
+                if (!$exclusive) {
+                    $form .= "<li>$property</li>";
+                    $form .= "<li>$value</li>";  
+                    $exclusive = true;
+                    continue;                  
+                }
+                $form .= "<ul>";
+                $form .= "<li>$property</li>";
+                $form .= "<input name='$property' value='$value'></input>";
+                $form .= "</ul>";
+            };
+            $form .= "<input type='submit' name='insert' value='Add record'></input>";
+            $form .= "<input type='hidden' name='target' value='Update'></input>";   
+            $form .=  "<input type='hidden' name='currentStudent' value='$studentNum'></input>";                      
+            $form .= "</form>";
+            echo $form;
+        };
+
         //Se usa en showForm para mantener los valores de los campos al regresar de showResults.
         function resumeState($valueName = null) {
                 if (!(isset($_SESSION[$valueName]))) {return;}
@@ -61,7 +122,7 @@
         function createTable($data) {
             $table = "<table style='text-align: center; margin: auto; width: auto;'>";
             foreach ($data as $property => $value) {
-                if ($property == "insert" || $property == "target" || $property == "state") {continue;};
+                if ($property == "insert" || $property == "target" || $property == "state" || $property == "currentStudent") {continue;};
                 $table.= "<tr><td>$property</td><td>$value</td></tr>";
             };
             $table .= "</table><br>";
@@ -153,24 +214,109 @@
             fwrite($database, date("d/m/Y+H:i:s") . "+1\r\n");
 			foreach ($_SESSION as $property => $value) {
 
-                if ($property == "insert" || $property == "target" || $property == "state") {continue;};
+                if ($property == "insert" || $property == "target" || $property == "state" || $property == "currentStudent") {continue;};
                 fwrite($database, "$property+$value\r\n");            
 		    };
             fclose($database);
             echo "Record successfully added to database!";
             $_SESSION["insert"] = null;
             session_unset();
+            echo "<form method='POST'>";
+                echo "<input type='submit' name='insert' value='Return'></input>";
+            echo "</form>";
+        };
+
+        function updateRecord($data, $currentStudent = null) {
+            $database = fopen("students.txt", "w");
+            foreach ($data as $student) {
+                $keys = array_keys($student);
+                $first = array_shift($keys);
+                $element = array_shift($student);
+                fwrite($database, "$first+$element+1\r\n");
+			    foreach ($student as $property => $value) {
+
+                    if ($property == "insert" || $property == "target" || $property == "state" || $property == "currentStudent") {continue;};
+                    fwrite($database, "$property+$value\r\n");            
+                };
+            };
+            fclose($database);
+            if ($_POST["insert"] == "Delete student") {
+                viewStudent(getStudentsData("students.txt"), $currentStudent);
+            } else {
+                echo "Record successfully updated to database!";
+                $_SESSION["insert"] = null;
+                session_unset();
+                echo "<form method='POST'>";
+                    echo "<input type='submit' name='insert' value='Return'></input>";
+                echo "</form>";
+            };
+        };
+
+        //Obtiene los datos de los estudiantes desde el registro en la forma:
+        //  $data[numeroEstudiante][propiedad] = valor
+        //(Los estudiantes son indexados desde 1, no desde 0)
+        function getStudentsData($filename) {
+            $database = fopen($filename, "r");
+            $data = array();
+            $studentNum = 0;
+            while (!feof($database)) {
+                $line = fgets($database);
+                $words = explode("+", $line);
+                //Esto se ejecuta al principio tambien, lo que provoca que se indexen desde el 1.
+                if (isset($words[2])) {
+                    $studentNum += 1;
+                };
+                if ($words[0] == "") {continue;};
+                if (!isset($words[1])) {continue;};
+                $data[$studentNum][$words[0]] = $words[1];
+            };
+            fclose($database);
+            return $data;
+        };
+
+        function chooseWrite($studentNum) {
+            $data = getStudentsData("students.txt");
+            foreach ($_SESSION as $property => $value) {
+                if ($property == "insert" || $property == "target" || $property == "state") {continue;};
+                $data[$studentNum][$property] = $value;
+            };
+            updateRecord($data);
+        };
+
+        function deleteStudent($studentNum) {
+            $data = getStudentsData("students.txt");
+            array_splice($data, $studentNum - 1, 1);
+            updateRecord($data, $studentNum);        
         };
 
         function main() {
+            if (!isset($_POST["currentStudent"])) {
+                $_POST["currentStudent"] = 1;
+            };
+            $currentStudent = $_POST["currentStudent"];
             if (isset($_POST["state"])) {
                 if ($_POST["state"] == "Show students") {
                     showStudents();
                 };
+                if ($_POST["state"] == "Anterior") {
+                    $currentStudent -= 1;
+                } elseif ($_POST["state"] == "Siguiente") {
+                    $currentStudent += 1;
+                };
             };
             if (isset($_POST["insert"]))  {
                 if ($_POST["insert"] == "Add record") {
+                    if (isset($_POST["target"]) && $_POST["target"] == "Update") {
+                        chooseWrite($currentStudent);
+                    }else {
                     addRecord();
+                    };
+                } elseif ($_POST["insert"] == "Update") {
+                    updateStudent(getStudentsData("students.txt"), $currentStudent);
+                } elseif ($_POST["insert"] == "Update students") {
+                    viewStudent(getStudentsData("students.txt"), $currentStudent);
+                } elseif ($_POST["insert"] == "Delete student") {
+                    deleteStudent($currentStudent);
                 } elseif ($_POST["insert"] == "Return") {
                     showForm();
                 } elseif ($_POST["insert"] == "Send form") {
